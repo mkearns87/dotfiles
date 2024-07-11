21dd987cd601a4cf713866e2e094fff55bc61def
Michael Kearns <milk@yelp.com>
2024-07-11 15:02:31 -0400

    derp

--- scripts/bootstrap/macos_bootstrap.sh
+++ scripts/bootstrap/macos_bootstrap.sh
@@ -74,7 +74,8 @@ setup_gnupg() {
   chmod 700 $GNUPGHOME
   touch "$GNUPGHOME/gpg-agent.conf"
   echo "enable-ssh-support" > "$GNUPGHOME/gpg-agent.conf"
-  gpg --keyserver hkps://keys.openpgp.org --recv-keys "$TRUSTED_GPGKEY_FINGREPRINT"
+  # gpg --keyserver hkps://keys.openpgp.org --recv-keys "$TRUSTED_GPGKEY_FINGREPRINT"
+  gpg --import $TEMP_PUBKEY
   echo "$TRUSTED_GPGKEY_FINGREPRINT:6:" | gpg --import-ownertrust
   gpg --card-status
   gpg --list-secret-keys
