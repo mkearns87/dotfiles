9e395a0970a12ae5f71649adf9f5593d2a035df6
Michael Kearns <milk@yelp.com>
2025-05-01 10:11:10 -0400

    Update macos_bootstrap.sh

--- scripts/bootstrap/macos_bootstrap.sh
+++ scripts/bootstrap/macos_bootstrap.sh
@@ -69,7 +69,7 @@ setup_gnupg() {
   echo "standard-resolver" >  "$GNUPGHOME/dirmngr.conf"
   pkill dirmngr
   sleep 3
-  gpg --keyserver hkps://keyserver.ubuntu.com/ --recv-keys "$TRUSTED_GPGKEY_FINGREPRINT"
+  gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys "$TRUSTED_GPGKEY_FINGREPRINT"
   echo "$TRUSTED_GPGKEY_FINGREPRINT:6:" | gpg --import-ownertrust
   gpg --card-status
   gpg --list-secret-keys
